2aad98af96be51e374d1feae2662ee9949f4eeaf
Michelle Yick <41213@longroad.ac.uk>
2014-10-19 18:08:14 +0100

    Iteration pre homework Task 5

--- Pre homework Task 5.py
+++ Pre homework Task 5.py
@@ -2,12 +2,12 @@
 #15-10-2014
 #Iteration pre homework task 5
 
-Result = 0
+result = False
+result = 0
 while not result:
     number = int(input("Enter a number:"))
     if number == 0:
         result = True
-    
     
 
 
