b3839e2d6aabd5cd7baeeddad62fa8fcc9820e20
41213 <41213@PC004141.coll.lr.local>
2014-10-22 14:53:16 +0100

    Completed class exercise

--- Class exercises (Revision Q3).py
+++ Class exercises (Revision Q3).py
@@ -1,11 +1,17 @@
 #Michelle Yick
-#21-10-2014
+#22-10-2014
 #Iteration class exercises. Revision Q3.
 
 #Program that asks the user the amount of numbers they want to be averaged and the actual numbers. ou then want to average them and display the average.
 
-amount = int(input("How mnay numbers would you like to average?:"))
-for count in range (1,amount+1):
-    numbers = int(input("Enter number to be averaged:"))
-    total = 
+total = 0
+amount = int(input("Enter the amount of numbers you want to average:"))
+
+for number in range(1,amount + 1):
+    number = int(input("Enter the number you want to be averaged:"))
+    total = total + number
+
+average = total/amount
+print("The average of the numbers you have entered is {0}".format(average))
+
     
