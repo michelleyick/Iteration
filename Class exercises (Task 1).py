98afc0ccdd4c5cae28c49bd046d87dfe62a2bfa7
Michelle Yick <41213@longroad.ac.uk>
2014-10-30 14:59:21 +0000

    Completed class exercise

--- Class exercises (Task 1).py
+++ Class exercises (Task 1).py
@@ -1,10 +1,9 @@
 #Michelle Yick
-#22-10-2014
+#30-10-2014
 #Iteration class exercises. Task 1.
 
-# program to prompt for 8 numbers and report the total to the user
-
 total = 0
+
 for number in range(1,9):
     number = int(input("Enter a number:"))
     total = total + number
