29b197119215674a66fbbcaff38be83c5bdf23a1
Michelle Yick <41213@longroad.ac.uk>
2014-10-30 14:40:04 +0000

    Completed class exercise

--- Class exercises (Revision Q5).py
+++ Class exercises (Revision Q5).py
@@ -1,14 +1,14 @@
 #Michelle Yick
-#28/10/2014
+#29/10/2014
 #Iteration class exercises. Revision Q5.
 
-total = 0
+total = 1
 numberSeries = 1
 amount = int(input("Amount of numbers you want to average"))
 
 while numberSeries != -1:
-    if numberSeries > 0:
-        numberSeries = int(input("Enter the series of numbers you wish to average. Seperate each number with a space."))
+    if numberSeries > 0 or numberSeries < -1 or numberSeries == 0:
+        numberSeries = int(input("Enter the number you wish to average"))
         total = numberSeries + total
 
 average = total/amount
