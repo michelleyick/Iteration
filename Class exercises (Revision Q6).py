e3fb7549d084d0ef74dd87e2e2d598d6e0a9b529
Michelle Yick <41213@longroad.ac.uk>
2014-11-01 17:23:14 +0000

    Completed class exercise

---
+++ Class exercises (Revision Q6).py
@@ -0,0 +1,11 @@
+#Michelle Yick
+#01/11/2014
+#Iteration class exercises. Revision Q6
+
+total = 2.2
+
+for pounds in range(1,21):
+                answer = pounds / total
+                print("{0: < 1} / {1} = {2}kg".format(pounds,total,answer))
+
+
