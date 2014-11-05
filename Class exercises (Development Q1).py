fd1a04664e78b1fe08fd6a807d2e19a9b1a1b213
41213 <41213@PC004141.coll.lr.local>
2014-11-05 15:04:53 +0000

    Completed class exercises

--- Class exercises (Development Q1).py
+++ Class exercises (Development Q1).py
@@ -1,10 +1,19 @@
 #Michelle Yick
-#05-11-2014
+#01-11-2014
 #Iteration class exercises. Development Q1.
 
 total = 1
 n = int(input("What number would you like to work out the factorial of?"))
 
-for nFactorial in range(1,n + 1):
-    total *= nFactorial
-    print(total)
+
+if n < 0:
+    print("Invalid value")
+elif n == 0:
+    print("The factorial of 0 is 1.")
+
+else:
+     for nFactorial in range(1,n + 1):
+        total *= nFactorial 
+        print(total)
+        
+
--- Class exercises (Revision Q6).py
+++ Class exercises (Revision Q6).py
@@ -6,6 +6,6 @@ total = 2.2
 
 for pounds in range(1,21):
                 answer = pounds / total
-                print("{0: < 1} / {1} = {2}kg".format(pounds,total,answer))
+                print("{0: < 2} / {1} = {2:.2f}kg".format(pounds,total,answer))
 
 
