61df01d8bc22786c200caa551075db517d77bbf7
41213 <41213@PC140032.coll.lr.local>
2014-11-05 11:12:15 +0000

    Development program that still needs to be tweaked

---
+++ Class exercises (Development Q1).py
@@ -0,0 +1,10 @@
+#Michelle Yick
+#05-11-2014
+#Iteration class exercises.Development Q1.
+
+total = 1
+n = int(input("What number would you like to work out the factorial of?"))
+
+for nFactorial in range(1,n + 1):
+    total *= nFactorial
+    print(total)
